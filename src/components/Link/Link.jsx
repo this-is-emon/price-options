6141e88b368687c7836f4533b17fcb2ff8f399d2
this-is-emon <mofijul.islam.emon@gmail.com>
2024-05-05 12:24:52 +0600

    added propType ,menubar icon, toggle state handler

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,9 +1,21 @@
+import PropTypes from "prop-types";
+
 const Link = ({ route }) => {
   return (
     <li className="mr-10">
       <a href={route.path}>{route.name}</a>
     </li>
   );
+};
+
+/*
+# - Runtime type checking for React props and similar objects.
+# - PropTypes provide type validation, prevent bugs, enhance collaboration, 
+    and contribute to better code quality in React applications
+*/
+
+Link.propTypes = {
+  route: PropTypes.object,
 };
 
 export default Link;
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,18 +1,29 @@
 // import React from 'react'
 
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import Link from "../Link/Link";
 
-const routes = [
-  { id: 1, name: "Home", path: "/" },
-  { id: 2, name: "About", path: "/about" },
-  { id: 3, name: "Servics", path: "/services" },
-  { id: 4, name: "Contact", path: "/contact" },
-  { id: 5, name: "Not Found", path: "*" },
-];
-
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const routes = [
+    { id: 1, name: "Home", path: "/" },
+    { id: 2, name: "About", path: "/about" },
+    { id: 3, name: "Servics", path: "/services" },
+    { id: 4, name: "Contact", path: "/contact" },
+    { id: 5, name: "Not Found", path: "*" },
+  ];
+
   return (
     <nav>
+      <div className="text-2xl md:hidden" onClick={() => setOpen(!open)}>
+        {open === true ? (
+          <AiOutlineClose></AiOutlineClose>
+        ) : (
+          <AiOutlineMenu className=""></AiOutlineMenu>
+        )}
+      </div>
       <ul className="md:flex">
         {routes.map((route) => (
           <Link key={route.id} route={route}></Link>
